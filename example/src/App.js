dfe06b1c6f25478e4970e7a7bd249d81d76efc20
Nhi PHAM <55150176+nhipham1995@users.noreply.github.com>
2024-02-01 17:34:17 +0100

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'demo-react-modal'
+import { Modal } from 'demo-react-modal'
 import 'demo-react-modal/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const onCloseHandler = () => {
+    setIsModalOpen(false)
+  }
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={onCloseHandler}
+        // isCloseBtn={true}
+        closeText={'Close'}
+        backgroundColor={'blue'}
+        textColor={'darkred'}
+      >
+        <p>
+          Employee Created!This HTML file is a template. If you open it directly
+          in the browser, you will see an empty page. You can add webfonts, meta
+          tags, or analytics to this file. The build step will place the bundled
+          scripts into the tag. To begin the development, run `npm start` or
+          `yarn start`. To create a production bundle, use `npm run build` or
+          `yarn build`.This HTML file is a template. If you open it directly in
+          the browser, you will see an empty page. You can add webfonts, meta
+          tags, or analytics to this file. The build step will place the bundled
+          scripts into the tag. To begin the development, run `npm start` or
+          `yarn start`. To create a production bundle, use `npm run build` or
+          `yarn build`.This HTML file is a template. If you open it directly in
+          the browser, you will see an empty page. You can add webfonts, meta
+          tags, or analytics to this file. The build step will place the bundled
+          scripts into thetag. To begin the development, run `npm start` or
+          `yarn start`. To create a production bundle, use `npm run build` or
+          `yarn build`. `yarn build`.This HTML file is a template. If you open
+          it directly in the browser, you will see an empty page. You can add
+          webfonts, meta tags, or analytics to this file. The build step will
+          place the bundled scripts into the tag. To begin the development, run
+          `npm start` or `yarn start`. To create a production bundle, use `npm
+          run build` or `yarn build`.This HTML file is a template. If you open
+          it directly in the browser, you will see an empty page. You can add
+          webfonts, meta tags, or analytics to this file. The build step will
+          place the bundled scripts into thetag. To begin the development, run
+          `npm start` or `yarn start`. To create a production bundle, use `npm
+          run build` or `yarn build`.
+          <span onClick={onCloseHandler} className='close-text'>
+            Close
+          </span>
+        </p>
+      </Modal>
+      <button onClick={() => setIsModalOpen(!isModalOpen)}>Open Modal</button>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,67 @@
 import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import { faX } from '@fortawesome/free-solid-svg-icons'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Modal = ({
+  isOpen,
+  onClose,
+  isCloseBtn,
+  backgroundColor,
+  textColor,
+  closeText,
+  children
+}) => {
+  const [prepareClose, setPrepareClose] = useState(false)
+
+  const onCloseHandler = () => {
+    setPrepareClose(true)
+    setTimeout(() => {
+      onClose(false)
+      setPrepareClose(false)
+    }, 800)
+  }
+
+  return (
+    isOpen && (
+      <div
+        className={[
+          styles.modal,
+          styles.modalFadeIn,
+          prepareClose && styles.modalFadeOut
+        ].join(' ')}
+        onClick={onCloseHandler}
+      >
+        <div
+          className={styles.modalContent}
+          style={{
+            backgroundColor: backgroundColor,
+            borderColor: backgroundColor,
+            color: textColor
+          }}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div>
+            <span
+              className={
+                closeText && isCloseBtn !== false
+                  ? [styles.close, styles.doubleClose].join(' ')
+                  : [styles.close, styles.simpleClose].join(' ')
+              }
+              onClick={onCloseHandler}
+            >
+              {isCloseBtn !== false && (
+                <FontAwesomeIcon icon={faX} style={{ paddingTop: '8px' }} />
+              )}
+              <p>{closeText}</p>
+            </span>
+          </div>
+
+          <div className={styles.content}> {children}</div>
+        </div>
+      </div>
+    )
+  )
 }
